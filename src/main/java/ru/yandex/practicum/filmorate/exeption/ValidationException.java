c946690746039b960fe306c17e1cbdf3257f4be2
Polina <cherkaschenkopo@gmail.com>
2023-04-29 16:07:40 +0300

    Предварительная настройка проекта java-filmorate

--- src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exeption;
 
 public class ValidationException extends RuntimeException {
-    public ValidationException (String message) {
+    public ValidationException(String message) {
         super(message);
     }
 }
